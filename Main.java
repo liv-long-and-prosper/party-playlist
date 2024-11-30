8a60eec997d7377f6a79605dfad9ca36508f4de4
liv-long-and-prosper <longolivia1129@gmail.com>
2024-11-30 01:06:52 -0800

    Final commit

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args){
 
-        // Adding my top 5 songs to an ArrayList
+        // Adding my top 5 songs to an ArrayList called myTopFive
         ArrayList<MusicTrack> myTopFive = new ArrayList<>(5);
         myTopFive.add(new MusicTrack("Nolan Taylor", "68"));
         myTopFive.add(new MusicTrack("John + Jane Q", "Watermelon"));
@@ -15,6 +15,7 @@ public class Main {
         myTopFive.add(new MusicTrack("Lin-Manuel Miranda;Hamilton","My Shot"));
         myTopFive.add(new MusicTrack("Zach Bryan","Fear and Friday's(Poem)"));
 
+        // Adding songs that I think my friends might like to an ArrayList called partyVibes
         ArrayList<MusicTrack> partyVibes = new ArrayList<>(6);
         partyVibes.add(new MusicTrack("Penelope Scott","Rät"));
         partyVibes.add(new MusicTrack("IC3PEAK,ZillaKami","TRRST"));
@@ -24,46 +25,58 @@ public class Main {
         partyVibes.add(new MusicTrack("Santana,Rob Thomas","Smooth"));
         partyVibes.remove(1);
 
+        // Merging myTopFive and partyVibes
         ArrayList<MusicTrack> finalPartyPlaylist = new ArrayList<>();
         finalPartyPlaylist.addAll(myTopFive);
         finalPartyPlaylist.addAll(partyVibes);
 
-        Iterator<MusicTrack> musicTrackIterator = finalPartyPlaylist.iterator();
+        // Shuffling songs in finalPartyPlaylist and printing
+        setTrackOrder(finalPartyPlaylist);
+        System.out.print("\n\n--------------\n");
+        System.out.print("Party Playlist\n");
+        System.out.print("--------------\n\n");
+        for (MusicTrack track : finalPartyPlaylist){
+            System.out.println(track);
+        }
+    }
+
+    /**
+     * GenerateRandomNum generates a random number between 0 and 100
+     *
+     * @return randomNum
+     * */
+    private static int generateRandomNum(){
+        int randomNum = (int) Math.floor(Math.random() * 100+1);
+        return randomNum;
+    }
+
+    /**
+     * SetTrackOrder takes a given ArrayList of MusicTrack objs and shuffles the tracks
+     *
+     * @param playlist      the ArrayList of MusicTrack objs
+     * @return              the shuffled ArrayList with trackOrder ascending from first to last track
+     * */
+    private static ArrayList<MusicTrack> setTrackOrder(ArrayList<MusicTrack> playlist){
+        if (playlist == null || playlist.size() == 0){
+            throw new IllegalArgumentException("playlist must not be null or empty, please provide a valid ArrayList<MusicTrack>");
+        }
+        // Create an Iterator to loop through playlist and setTrackOrder to a random int between 0 & 100
+        Iterator<MusicTrack> musicTrackIterator = playlist.iterator();
         while (musicTrackIterator.hasNext()){
             MusicTrack currentTrack = musicTrackIterator.next();
             currentTrack.setTrackOrder(generateRandomNum());
         }
-        Collections.sort(finalPartyPlaylist);
 
-        System.out.print("\n\n--------------\n");
-        System.out.print("Party Playlist\n");
-        System.out.print("--------------\n");
-        int order = 1;
-        for(MusicTrack track : finalPartyPlaylist){
+        // Sort playlist
+        Collections.sort(playlist);
+
+        // Use an index-based for-loop to set the track order in ascending order from first to last
+       int order = 1;
+        for(int i = 0; i < playlist.size(); i++){
+            MusicTrack track = playlist.get(i);
             track.setTrackOrder(order);
-            System.out.println(track);
             order++;
         }
-    }
-
-//    private static void sortByArtist(ArrayList<MusicTrack> playlist){
-//
-//    }
-//    private static void sortBySong(ArrayList<MusicTrack> playlist){
-//
-//    }
-
-//    private static void sortByTrackOrder(Iterator<MusicTrack> musicTrackIterator){
-//        int order = 1;
-//        while (musicTrackIterator.hasNext()){
-//            MusicTrack currentTrack = musicTrackIterator.next();
-//            currentTrack.setTrackOrder(order);
-//            order++;
-//        }
-//    }
-
-    private static int generateRandomNum(){
-        int randomNum = (int) Math.floor(Math.random() * 100+1);
-        return randomNum;
+        return playlist;
     }
 }
--- MusicTrack.java
+++ MusicTrack.java
@@ -7,9 +7,6 @@
 
 package PartyPlaylist;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class MusicTrack implements Comparable<MusicTrack> {
 
     //---------------------------------------------------------------
@@ -20,18 +17,18 @@ public class MusicTrack implements Comparable<MusicTrack> {
     private int trackOrder;
 
     //---------------------------------------------------------------
-    //      CONSTRUCTORS
+    //      CONSTRUCTOR
     //---------------------------------------------------------------
+
+    /**
+     * MusicTrack Constructor
+     *
+     * @param artist    the artist's name
+     * @param songTitle the title of the track
+     * */
     public MusicTrack(String artist, String songTitle){
         setArtist(artist);
         setSongTitle(songTitle);
-        setTrackOrder(1);
-    }
-
-    public MusicTrack(String artist, String songTitle, int trackOrder){
-        setArtist(artist);
-        setSongTitle(songTitle);
-        setTrackOrder(trackOrder);
     }
 
     //---------------------------------------------------------------
@@ -52,21 +49,45 @@ public class MusicTrack implements Comparable<MusicTrack> {
     //---------------------------------------------------------------
     //      SET METHODS
     //---------------------------------------------------------------
+
+    /**
+     * SetArtist sets the artist's name
+     *
+     * @param artist    the artist's name; must not be null or emtpy
+     * @throws IllegalArgumentException     if artist is null or emtpy
+     * */
     public void setArtist(String artist){
+        if(artist == null || artist.isBlank()){
+            throw new IllegalArgumentException("artist must not be null or empty, please provide a valid String");
+        }
         this.artist = artist;
     }
 
+    /**
+     * SetSongTitle sets the title of the track
+     *
+     * @param songTitle    the title of the track; must not be null or emtpy
+     * @throws IllegalArgumentException     if songTitle is null or emtpy
+     * */
     public void setSongTitle(String songTitle){
+        if (songTitle == null || songTitle.isBlank()){
+            throw new IllegalArgumentException("songTitle must not be null or empty, please provide a valid String");
+        }
         this.songTitle = songTitle;
     }
 
+    /**
+     * SetTrackOrder sets the placement of the song in a playlist
+     *
+     * @param trackOrder    the placement of the song in a playlist
+     * @throws IllegalArgumentException     if trackOrder is less than or equal to zero
+     * */
     public void setTrackOrder(int trackOrder){
+        if (trackOrder <= 0){
+            throw new IllegalArgumentException("trackOrder must be greater than 0, please provide a valid integer");
+        }
         this.trackOrder = trackOrder;
     }
-
-    //---------------------------------------------------------------
-    //      STATIC METHODS
-    //---------------------------------------------------------------
 
     //---------------------------------------------------------------
     //      OTHER METHODS
@@ -75,6 +96,9 @@ public class MusicTrack implements Comparable<MusicTrack> {
         return String.format("%d. %s -- %s\n",trackOrder,songTitle,artist);
     }
 
+    /**
+     * {@inheritDoc}
+     * */
     @Override
     public int compareTo(MusicTrack other) {
         return Integer.compare(this.trackOrder,other.trackOrder);
